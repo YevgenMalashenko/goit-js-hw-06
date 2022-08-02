5af73d6900c63af9442f2b1d402f76bbcb1ab684
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-02 17:45:59 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,31 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+ingredients.forEach((el) => {
+  const liItem = document.createElement("li");
+  liItem.textContent = el;
+  liItem.classList.add("item");
+  listEl.prepend(liItem);
+});
+
+//!  4. После чего вставит все <li> за одну операцию в список ul#ingredients - не удовлетворяет, так как елементі вставляются не за одну операцию
+
+// const listEl = document.querySelector("#ingredients");
+
+// let liItem;
+
+// ingredients.forEach((el) => {
+//   const liItem = document.createElement("li");
+//   liItem.textContent = el;
+//   liItem.classList.add("item");
+// });
+
+// listEl.prepend(liItem);
