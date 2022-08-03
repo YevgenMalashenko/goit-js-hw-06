9acd71ba3b3f4033df07995dbd45b902c22d49f5
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-03 19:33:24 +0300

    finish task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButtonElement = document.querySelector(".change-color");
+
+const colorNameEl = document.querySelector(".color");
+console.log(colorNameEl);
+
+const bodyEl = document.querySelector("body");
+
+changeColorButtonElement.addEventListener(
+  "click",
+  handleChangeColorButtonElement
+);
+
+function handleChangeColorButtonElement() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = getRandomHexColor();
+}
