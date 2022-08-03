863099a7c2b358eceabfc78be93d79926f60d89e
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-03 19:09:23 +0300

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginFormElement = document.querySelector(".login-form");
+// console.log(loginFormElement);
 
+const msg = "dlaskfjadjks";
+loginFormElement.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  // console.dir(formElements);
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === "" || password === "");
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
